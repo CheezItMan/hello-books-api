ee7b4977d1b91b434717903a1fd417249a3193ed
Chris McAnally <chris@adadevelopersacademy.org>
2021-04-20 18:36:28 -0700

    fix broken endpoint

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,13 @@ def hello_world_json():
     }, 404
 
 
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"] + [new_hobby]
+    return response_body
